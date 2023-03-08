a4fd32f890a5fbdfae213a8802d6cafad13d93cd
luisenrique21 <92737337+luisenrique21@users.noreply.github.com>
2023-03-07 20:50:55 -0600

    TAREA 2

--- WebApiComputadoras/BD/Computadoras.cs
+++ WebApiComputadoras/BD/Computadoras.cs
@@ -1,9 +1,16 @@
-﻿namespace WebApiComputadora.BD
+﻿namespace WebApicomputadoras.BD
 {
     public class Computadoras
     {
         public int Id { get; set; }
+
         public string Nombre { get; set; }
 
+        public string Marca { get; set; }
+
+        public int TiendaID { get; set; }
+
+        public int Precio { get; set; }
+        public List<Tiendas> Tienda { get; set; }
     }
 }
---
+++ WebApiComputadoras/BD/Tiendas.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApicomputadoras.BD
+{
+    public class Tiendas
+    {
+        public int Id { get ; set; }
+        
+        public string Nombre { get; set; }
+        
+        public string Direccion { get; set;}
+        
+        public int Precio { get; set; }
+        
+        public int ComputadoraID { get; set; }
+        public Computadoras Computadoras { get; set; }
+    }
+}
