11cc1235fe8b6e765cbe40d3852b836700417308
luisenrique21 <92737337+luisenrique21@users.noreply.github.com>
2023-03-07 20:49:06 -0600

    TAREA 1

---
+++ WebApiComputadoras/AplicationBDContext.cs
@@ -0,0 +1,14 @@
+﻿using WebApiComputadora.BD;
+
+namespace WebApiComputadora
+{
+    public class AplicationBDContext : DbContext
+    {
+        public AplicationBDContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Computadoras> Computadoras { get; set; }
+    }
+}
