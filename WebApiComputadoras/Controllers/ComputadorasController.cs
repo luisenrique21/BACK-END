75c3860602b97829345739470c7cf55082a1ca00
luisenrique21 <92737337+luisenrique21@users.noreply.github.com>
2023-03-07 20:47:45 -0600

    TAREA 1

---
+++ WebApiComputadoras/Controllers/ComputadorasController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiComputadora.BD;
+
+namespace WebApiComputadora.Controllers
+{
+    [ApiController]
+    [Route("api/computadoras")]
+    public class ComputadorasController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Computadoras>> Get() 
+        {
+            return new List<Computadoras>()
+            {
+                new Computadoras { Id=1, Nombre= "PC GAMER MASTER RACE"},
+                new Computadoras {Id=2, Nombre= "PC ASUS VIVOBOOK S15"}
+            };
+        }
+    }
+}
